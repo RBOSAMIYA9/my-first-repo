e300e8485c7445e76535441bbefd69a67dcd4ad9
Ravindra Bosamiya <rbosamiya9@gmail.com>
2023-04-01 08:05:45 +0530

    Added : script file

---
+++ script.js
@@ -0,0 +1,31 @@
+// const elementById = document.getElementById("heading");
+// console.log("element by Id", elementById);
+
+// let rates = document.getElementsByName("rate");
+// rates.forEach((rate) => {
+//   if (rate.checked) {
+//     output.innerText = `You selected: ${rate.value}`;
+//   }
+// });
+
+// const elementByClassName = document.getElementsByClassName("random-para");
+// console.log("element by className", elementByClassName);
+
+// let headings = document.getElementsByTagName('h1');
+// console.log(`The number of H1 tags: ${headings.length}`);
+
+// let element = parentNode.querySelector(selector);
+
+//universal selector
+// let element = document.querySelector('*');
+
+// let firstHeading = document.querySelector('h1');
+
+//for class name
+// let className = document.querySelector('.random-para');
+
+//for id
+//  let className = document.querySelector('.#id');
+
+
+
